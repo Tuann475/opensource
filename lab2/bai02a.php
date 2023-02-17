a76b997f6faf1fc8c1f684b6b8e0db72ed36f6f5
pham tuan <89393220+P-TUAN@users.noreply.github.com>
2023-02-17 08:40:19 +0700

    Add files via upload

---
+++ lab2/bai02a.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>  
+ 
+</style>   
+</head>
+<body>
+
+<title>Introduction of Cats</title>
+<center>
+<table background="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM8jH34diHSXsX12n_-hBkgg_kMyn7dpX80eFUQH-G74Cjpqpbsj7355lL7cIQUzm1tKo&usqp=CAU" border="1px" width="550px" align="center">
+<tr align="center">
+	<td colspan="12"<h1>Sản phẩm giỏ hoa khuyến mãi</h1></td></tr>
+	<tr align="center">
+<tr align=“Center”>
+<td Colspan=“4”><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpkJbAD8uhfquj6AUsojN6zQ-_ui3bZKdIYU7w5K4UzcepF9rdEqt3PtLEhVpWG2Lidzg&usqp=CAU" alt=""></td>
+<td Colspan=“4”><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM8jH34diHSXsX12n_-hBkgg_kMyn7dpX80eFUQH-G74Cjpqpbsj7355lL7cIQUzm1tKo&usqp=CAU" alt=""></td>
+<td Colspan=“4”><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM8jH34diHSXsX12n_-hBkgg_kMyn7dpX80eFUQH-G74Cjpqpbsj7355lL7cIQUzm1tKo&usqp=CAU" alt=""></td>
+<td Colspan=“4”><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqs_dzRJZpMxHQbok6Znvt-J-GtPB37yC5rvEVmymfs7TEad7TnO7e3zNzX3Z7yjR-GuI&usqp=CAU" alt=""></td></tr>
+<tr align=“Center”>
+<td Colspan=“4”><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpkJbAD8uhfquj6AUsojN6zQ-_ui3bZKdIYU7w5K4UzcepF9rdEqt3PtLEhVpWG2Lidzg&usqp=CAU" alt=""></td>
+<td Colspan=“4”><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM8jH34diHSXsX12n_-hBkgg_kMyn7dpX80eFUQH-G74Cjpqpbsj7355lL7cIQUzm1tKo&usqp=CAU" alt=""></td>
+<td Colspan=“4”><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM8jH34diHSXsX12n_-hBkgg_kMyn7dpX80eFUQH-G74Cjpqpbsj7355lL7cIQUzm1tKo&usqp=CAU" alt=""></td>
+<td Colspan=“4”><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqs_dzRJZpMxHQbok6Znvt-J-GtPB37yC5rvEVmymfs7TEad7TnO7e3zNzX3Z7yjR-GuI&usqp=CAU" alt=""></td>
+</tr>
+</table>
+</center>
+
+</body>
+</html>
