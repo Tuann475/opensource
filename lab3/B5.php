156647ee214117f204528007434dcc89169d85d7
pham tuan <89393220+P-TUAN@users.noreply.github.com>
2023-02-25 17:40:59 +0700

    Add files via upload

---
+++ lab3/B5.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    .thangduong {
+    text-align: center;
+    font-size: 150%;
+    font-weight: bold;
+    color: #330033;
+    background-color: #CCFFCC;
+}
+.ngayduong {
+    text-align: center;
+    font-size: 400%;
+    font-weight: bold;
+}
+.ngayam {
+    text-align: center;
+    font-size: 160%;
+    color: black;
+}
+.gioam {
+    text-align: center;
+    font-size: 80%;
+    color: black;
+}
+.thangam {
+    text-align: center;
+    font-size: 80%;
+    color: black;
+}
+user agent stylesheet
+div {
+    display: block;
+}
+.thang {
+    font-size: 11pt;
+    padding: 1;
+    font-family: Tahoma,Verdana,Arial;
+    table-layout: fixed;
+}
+.thuduong {
+    text-align: center;
+    font-size: 125%;
+    font-weight: bold;
+    text-decoration: underline;
+}
+.ngaytuan {
+    width: 14%;
+    text-align: center;
+    font-weight: bold;
+    font-size: 110%;
+    line-height: 100%;
+    color: #330033;
+    background-color: #FFFFCC;
+}
+.am {
+    text-align: right;
+    font-size: 80%;
+    line-height: 80%;
+    color: blue;
+}
+form {
+    display: block;
+    margin-top: 0em;
+    margin-block-end: 1em;
+}
+.navi-l {
+    text-align: center;
+    font-size: 100%;
+    line-height: 100%;
+    font-weight: bold;
+    background-color: #CCCC99;
+}
+
+</style>
+<body>
+<body>
+
+<!--
+<iframe width=800 height=600 src="amlich.php"><iframe>
+-->
+
+<table align="center" width="400" border="1" cellpadding="0" cellspacing="0">
+<tbody><tr>
+<td>
+
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="Author" content="Ho Ngoc Duc">
+<title>Âm lịch Việt Nam @ Hồ Ngọc Đức</title>
+<script language="JavaScript" src="amlich.js" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="amlich.css">
+
+
+
+
+<table border="0" width="100%">
+
+<tbody><tr>
+<td>
+	<table class="thang" width="100%" border="0" cellpadding="1" cellspacing="1">
+	<tbody><tr>
+	<td colspan="2" id="thangduong" class="thangduong">Tháng 8 năm 2022</td>
+	</tr>
+	<tr>
+	<td colspan="2" id="ngayduong" class="ngayduong">24</td>
+	</tr>
+	<tr>
+	<td colspan="2" id="thuduong" class="thuduong">Thứ Năm</td>
+	</tr>
+
+	<tr>
+	<td> 
+	<div id="thangam" class="thangam">Tháng Bảy (T)</div>
+	<div id="ngayam" class="ngayam">28</div>
+	<div id="namam" class="thangam">Năm Nhâm Dần</div>
+	</td>
+	<td class="canchi">
+	<div id="canchithang" class="gioam">Tháng Mậu Thân</div>
+	<div id="canchingay" class="gioam">Ngày Canh Tuất</div>
+	<div id="canchigio" class="gioam">Giờ Bính Tý</div>
+	<div id="tietkhi" class="gioam">Tiết Xử thử</div>
+	</td>
+	</tr>
+
+	<tr>
+	<td colspan="2" id="dayinfo" class="info">&nbsp;</td>
+	</tr>
+
+	<tr>
+	<td colspan="2" id="hoangdao" class="hoangdao">Giờ hoàng đạo: Dần (3-5), Thìn (7-9), Tỵ (9-11), Thân (15-17), Dậu (17-19), Hợi (21-23)</td>
+	</tr>
+
+	</tbody></table>
+</td>
+</tr>
+
+<tr>
+<td>
+	<table class="thang" border="1" cellpadding="2" cellspacing="2" width="100%">
+	
+	<tbody><tr class="ngaytuan">
+	<td>CN</td> <td>T2</td> <td>T3</td> <td>T4</td> <td>T5</td> <td>T6</td> <td>T7</td>
+	</tr>
+
+	<tr class="normal">
+<td class="ngaythang" id="cell0"><div class="cn">&nbsp;</div><div class="am">&nbsp;</div></td>
+<td class="ngaythang" id="cell1" title="Thứ Hai 1/8/2022 - 4/7 Nhâm Dần" onclick="showDayInfo(1,4,7,2022,0,29,2459793,1,8,2022);"><div class="t2t6">1</div><div class="am">4/7T</div></td>
+<td class="ngaythang" id="cell2" title="Thứ Ba 2/8/2022 - 5/7 Nhâm Dần" onclick="showDayInfo(2,5,7,2022,0,29,2459794,2,8,2022);"><div class="t2t6">2</div><div class="am">5</div></td>
+<td class="ngaythang" id="cell3" title="Thứ Tư 3/8/2022 - 6/7 Nhâm Dần" onclick="showDayInfo(3,6,7,2022,0,29,2459795,3,8,2022);"><div class="t2t6">3</div><div class="am">6</div></td>
+<td class="ngaythang" id="cell4" title="Thứ Năm 4/8/2022 - 7/7 Nhâm Dần" onclick="showDayInfo(4,7,7,2022,0,29,2459796,4,8,2022);"><div class="t2t6">4</div><div class="am">7</div></td>
+<td class="ngaythang" id="cell5" title="Thứ Sáu 5/8/2022 - 8/7 Nhâm Dần" onclick="showDayInfo(5,8,7,2022,0,29,2459797,5,8,2022);"><div class="t2t6">5</div><div class="am">8</div></td>
+<td class="ngaythang" id="cell6" title="Thứ Bảy 6/8/2022 - 9/7 Nhâm Dần" onclick="showDayInfo(6,9,7,2022,0,29,2459798,6,8,2022);"><div class="t7">6</div><div class="am">9</div></td>
+</tr>
+<tr class="normal">
+<td class="ngaythang" id="cell7" title="Chủ Nhật 7/8/2022 - 10/7 Nhâm Dần" onclick="showDayInfo(7,10,7,2022,0,29,2459799,7,8,2022);"><div class="cn">7</div><div class="am">10</div></td>
+<td class="ngaythang" id="cell8" title="Thứ Hai 8/8/2022 - 11/7 Nhâm Dần" onclick="showDayInfo(8,11,7,2022,0,29,2459800,8,8,2022);"><div class="t2t6">8</div><div class="am">11</div></td>
+<td class="ngaythang" id="cell9" title="Thứ Ba 9/8/2022 - 12/7 Nhâm Dần" onclick="showDayInfo(9,12,7,2022,0,29,2459801,9,8,2022);"><div class="t2t6">9</div><div class="am">12</div></td>
+<td class="ngaythang" id="cell10" title="Thứ Tư 10/8/2022 - 13/7 Nhâm Dần" onclick="showDayInfo(10,13,7,2022,0,29,2459802,10,8,2022);"><div class="t2t6">10</div><div class="am">13</div></td>
+<td class="ngaythang" id="cell11" title="Thứ Năm 11/8/2022 - 14/7 Nhâm Dần" onclick="showDayInfo(11,14,7,2022,0,29,2459803,11,8,2022);"><div class="t2t6">11</div><div class="am">14</div></td>
+<td class="ngaythang" id="cell12" title="Thứ Sáu 12/8/2022 - 15/7 Nhâm Dần - Vu Lan (Rằm tháng 7)" onclick="showDayInfo(12,15,7,2022,0,29,2459804,12,8,2022);"><div class="t2t6">12 *</div><div class="am">15</div></td>
+<td class="ngaythang" id="cell13" title="Thứ Bảy 13/8/2022 - 16/7 Nhâm Dần" onclick="showDayInfo(13,16,7,2022,0,29,2459805,13,8,2022);"><div class="t7">13</div><div class="am">16</div></td>
+</tr>
+<tr class="normal">
+<td class="ngaythang" id="cell14" title="Chủ Nhật 14/8/2022 - 17/7 Nhâm Dần" onclick="showDayInfo(14,17,7,2022,0,29,2459806,14,8,2022);"><div class="cn">14</div><div class="am">17</div></td>
+<td class="ngaythang" id="cell15" title="Thứ Hai 15/8/2022 - 18/7 Nhâm Dần" onclick="showDayInfo(15,18,7,2022,0,29,2459807,15,8,2022);"><div class="t2t6">15</div><div class="am">18</div></td>
+<td class="ngaythang" id="cell16" title="Thứ Ba 16/8/2022 - 19/7 Nhâm Dần" onclick="showDayInfo(16,19,7,2022,0,29,2459808,16,8,2022);"><div class="t2t6">16</div><div class="am">19</div></td>
+<td class="ngaythang" id="cell17" title="Thứ Tư 17/8/2022 - 20/7 Nhâm Dần" onclick="showDayInfo(17,20,7,2022,0,29,2459809,17,8,2022);"><div class="t2t6">17</div><div class="am">20</div></td>
+<td class="ngaythang" id="cell18" title="Thứ Năm 18/8/2022 - 21/7 Nhâm Dần" onclick="showDayInfo(18,21,7,2022,0,29,2459810,18,8,2022);"><div class="t2t6">18</div><div class="am">21</div></td>
+<td class="ngaythang" id="cell19" title="Thứ Sáu 19/8/2022 - 22/7 Nhâm Dần" onclick="showDayInfo(19,22,7,2022,0,29,2459811,19,8,2022);"><div class="t2t6">19</div><div class="am">22</div></td>
+<td class="ngaythang" id="cell20" title="Thứ Bảy 20/8/2022 - 23/7 Nhâm Dần" onclick="showDayInfo(20,23,7,2022,0,29,2459812,20,8,2022);"><div class="t7">20</div><div class="am">23</div></td>
+</tr>
+<tr class="normal">
+<td class="ngaythang" id="cell21" title="Chủ Nhật 21/8/2022 - 24/7 Nhâm Dần" onclick="showDayInfo(21,24,7,2022,0,29,2459813,21,8,2022);"><div class="cn">21</div><div class="am">24</div></td>
+<td class="ngaythang" id="cell22" title="Thứ Hai 22/8/2022 - 25/7 Nhâm Dần" onclick="showDayInfo(22,25,7,2022,0,29,2459814,22,8,2022);"><div class="t2t6">22</div><div class="am">25</div></td>
+<td class="ngaythang" id="cell23" title="Thứ Ba 23/8/2022 - 26/7 Nhâm Dần" onclick="showDayInfo(23,26,7,2022,0,29,2459815,23,8,2022);"><div class="t2t6">23</div><div class="am">26</div></td>
+<td class="ngaythang" id="cell24" title="Thứ Tư 24/8/2022 - 27/7 Nhâm Dần" onclick="showDayInfo(24,27,7,2022,0,29,2459816,24,8,2022);"><div class="t2t6">24</div><div class="am">27</div></td>
+<td class="homnay" id="cell25" title="Thứ Năm 25/8/2022 - 28/7 Nhâm Dần" onclick="showDayInfo(25,28,7,2022,0,29,2459817,25,8,2022);"><div class="t2t6">25</div><div class="am">28</div></td>
+<td class="ngaythang" id="cell26" title="Thứ Sáu 26/8/2022 - 29/7 Nhâm Dần" onclick="showDayInfo(26,29,7,2022,0,29,2459818,26,8,2022);"><div class="t2t6">26</div><div class="am">29</div></td>
+<td class="ngaythang" id="cell27" title="Thứ Bảy 27/8/2022 - 1/8 Nhâm Dần" onclick="showDayInfo(27,1,8,2022,0,30,2459819,27,8,2022);"><div class="t7">27</div><div class="am">1/8Đ</div></td>
+</tr>
+<tr class="normal">
+<td class="ngaythang" id="cell28" title="Chủ Nhật 28/8/2022 - 2/8 Nhâm Dần" onclick="showDayInfo(28,2,8,2022,0,30,2459820,28,8,2022);"><div class="cn">28</div><div class="am">2</div></td>
+<td class="ngaythang" id="cell29" title="Thứ Hai 29/8/2022 - 3/8 Nhâm Dần" onclick="showDayInfo(29,3,8,2022,0,30,2459821,29,8,2022);"><div class="t2t6">29</div><div class="am">3</div></td>
+<td class="ngaythang" id="cell30" title="Thứ Ba 30/8/2022 - 4/8 Nhâm Dần" onclick="showDayInfo(30,4,8,2022,0,30,2459822,30,8,2022);"><div class="t2t6">30</div><div class="am">4</div></td>
+<td class="ngaythang" id="cell31" title="Thứ Tư 31/8/2022 - 5/8 Nhâm Dần" onclick="showDayInfo(31,5,8,2022,0,30,2459823,31,8,2022);"><div class="t2t6">31</div><div class="am">5</div></td>
+<td class="ngaythang" id="cell32"><div class="cn">&nbsp;</div><div class="am">&nbsp;</div></td>
+<td class="ngaythang" id="cell33"><div class="cn">&nbsp;</div><div class="am">&nbsp;</div></td>
+<td class="ngaythang" id="cell34"><div class="cn">&nbsp;</div><div class="am">&nbsp;</div></td>
+</tr>
+<tr class="normal">
+<td class="ngaythang" id="cell35"><div class="cn">&nbsp;</div><div class="am">&nbsp;</div></td>
+<td class="ngaythang" id="cell36"><div class="cn">&nbsp;</div><div class="am">&nbsp;</div></td>
+<td class="ngaythang" id="cell37"><div class="cn">&nbsp;</div><div class="am">&nbsp;</div></td>
+<td class="ngaythang" id="cell38"><div class="cn">&nbsp;</div><div class="am">&nbsp;</div></td>
+<td class="ngaythang" id="cell39"><div class="cn">&nbsp;</div><div class="am">&nbsp;</div></td>
+<td class="ngaythang" id="cell40"><div class="cn">&nbsp;</div><div class="am">&nbsp;</div></td>
+<td class="ngaythang" id="cell41"><div class="cn">&nbsp;</div><div class="am">&nbsp;</div></td>
+</tr>
+
+	</tbody></table>
+
+</td>
+</tr>
+
+</tbody></table>
+
+<div class="navi-l">
+	<form name="NaviForm" method="post">
+	<a href="?dd=25&amp;mm=8&amp;yy=2021" title="Năm trước">&lt;&lt;</a> &nbsp;
+	<a href="?dd=25&amp;mm=7&amp;yy=2022" title="Tháng trước">&lt;</a>	&nbsp;
+
+	<select name="dd">
+		<option value="1">1
+	</option><option value="2">2
+	</option><option value="3">3
+	</option><option value="4">4
+	</option><option value="5">5
+	</option><option value="6">6
+	</option><option value="7">7
+	</option><option value="8">8
+	</option><option value="9">9
+	</option><option value="10">10
+	</option><option value="11">11
+	</option><option value="12">12
+	</option><option value="13">13
+	</option><option value="14">14
+	</option><option value="15">15
+	</option><option value="16">16
+	</option><option value="17">17
+	</option><option value="18">18
+	</option><option value="19">19
+	</option><option value="20">20
+	</option><option value="21">21
+	</option><option value="22">22
+	</option><option value="23">23
+	</option><option value="24">24
+	</option><option value="25" selected="">25
+	</option><option value="26">26
+	</option><option value="27">27
+	</option><option value="28">28
+	</option><option value="29">29
+	</option><option value="30">30
+	</option><option value="31">31
+	</option></select>/
+	<select name="mm">
+		<option value="1">1
+	</option><option value="2">2
+	</option><option value="3">3
+	</option><option value="4">4
+	</option><option value="5">5
+	</option><option value="6">6
+	</option><option value="7">7
+	</option><option value="8" selected="">8
+	</option><option value="9">9
+	</option><option value="10">10
+	</option><option value="11">11
+	</option><option value="12">12
+	</option></select>
+	/ <input name="yy" size="3" value="2022"> 
+	<input type="submit" value="OK">
+	&nbsp;
+	<a href="?dd=25&amp;mm=9&amp;yy=2022" title="Tháng sau">&gt;</a> 
+	&nbsp;
+	<a href="?dd=25&amp;mm=8&amp;yy=2023" title="Năm sau">&gt;&gt;</a>
+	&nbsp;&nbsp;
+	<a href="?dd=25&amp;mm=2&amp;yy=2023" title="Ngày hôm nay">@</a> 
+	&nbsp;
+	</form>
+
+</div>
+
+
+
+</td>
+</tr>
+</tbody></table>
+</body>
+</html>
